030a75cea599118e8a20ac86345f9f71857609f2
zhuyixin43 <zhuyixin@github.com>
2023-07-21 11:37:48 -0700

    merged all bam files for 1 species, re-ran the extraction process

--- code/igv.sh
+++ code/igv.sh
@@ -4,7 +4,7 @@ function igv {
     do
     echo $sv
     mkdir /home1/zhuyixin/sc1/ImmAssm/igv/snapshot
-    bam="/home1/zhuyixin/sc1/ImmAssm/aligned_bam/mCanLor1/m64016_200910_161534_mapping_sorted.bam"
+    bam="/home1/zhuyixin/sc1/ImmAssm/aligned_bam/mCanLor1/mCanLor1_merged.bam"
     echo "new"
     echo "preference SAM.SHOW_ALL_BASES 0"
     echo "genome /home1/zhuyixin/sc1/ImmAssm/assemblies/mCanLor1.pri.cur.20210315.fasta"
@@ -12,7 +12,7 @@ function igv {
     echo "load ${bam}"
     echo "load /home1/zhuyixin/sc1/ImmAssm/gene_position/mCanLor1/gene_IGH_pos_sorted.bed"
     echo "goto ${chrom}:${start}-${end}"
-    echo "snapshot m64016_200910_161534_${sv}.png"
+    echo "snapshot mCanLor1_merged_${sv}.png"
     done    > /home1/zhuyixin/sc1/ImmAssm/igv/igv.txt
 }
 
