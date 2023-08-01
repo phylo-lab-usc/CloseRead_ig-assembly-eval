65b43700cc9823d9d07344040c32515565a98f65
zhuyixin43 <zhuyixin@github.com>
2023-08-01 10:17:05 -0700

    adding functional/nonfunctional option to the automation process

--- code/geneLociAutomated.sh
+++ code/geneLociAutomated.sh
@@ -18,34 +18,47 @@ conda activate /home1/zhuyixin/.conda/envs/assembly
 
 
 HOME=/home1/zhuyixin/sc1/ImmAssm
-
-cat ${HOME}/code/name.txt | while read line
+while getopts f: flag
+do
+    case "${flag}" in
+        f) function=${OPTARG};;
+    esac
+done
+cat ${HOME}/igv_name.txt | while read line
 do 
     #create output directories
-    mkdir /home1/zhuyixin/sc1/ImmAssm/gene_position/${line}
+    mkdir /home1/zhuyixin/sc1/ImmAssm/gene_position/${function}/${line}
     #Select only the productive gene
-    awk -F$'\t' '$6 == "True"' ${HOME}/mammalian_igdetective_v2.0/${line}_igdetective/combined_genes_IGH.txt > ${HOME}/gene_position/${line}/${line}_genes_IGH_productive.txt
+    awk -F$'\t' '$6 == "True"' ${HOME}/mammalian_igdetective_v2.0/${line}_igdetective/combined_genes_IGH.txt > ${HOME}/gene_position/${function}/${line}/${line}_genes_IGH_productive.txt
+    #Select only the non-productive gene
+    awk -F$'\t' '$6 == "False"' ${HOME}/mammalian_igdetective_v2.0/${line}_igdetective/combined_genes_IGH.txt > ${HOME}/gene_position/${function}/${line}/${line}_genes_IGH_nonproductive.txt
+    #save as variable
+    if [ "$function" = "functional" ]; then
+        IGHloci=${line}_genes_IGH_productive.txt
+    else
+        IGHloci=${line}_genes_IGH_nonproductive.txt
+    fi
     #Count each gene's length
-    awk -F$'\t' '{print $5}' ${HOME}/gene_position/${line}/${line}_genes_IGH_productive.txt | awk -v FS="" '{print NF;}' > ${HOME}/gene_position/${line}/${line}_genes_IGH_productive_length.txt
+    awk -F$'\t' '{print $5}' ${HOME}/gene_position/${function}/${line}/${IGHloci} | awk -v FS="" '{print NF;}' > ${HOME}/gene_position/${function}/${line}/${line}_genes_IGH_length.txt
     #add a header "Length"
-    sed -i  "1s/.*/Length/" ${HOME}/gene_position/${line}/${line}_genes_IGH_productive_length.txt 
+    sed -i  "1s/.*/Length/" ${HOME}/gene_position/${function}/${line}/${line}_genes_IGH_length.txt 
     #get Chromosome, start, strand infomation
-    awk -F$'\t' '{print $2 "\t" $3 "\t" $4}' ${HOME}/gene_position/${line}/${line}_genes_IGH_productive.txt > ${HOME}/gene_position/${line}/gene_IGH_start.txt
+    awk -F$'\t' '{print $2 "\t" $3 "\t" $4}' ${HOME}/gene_position/${function}/${line}/${IGHloci} > ${HOME}/gene_position/${function}/${line}/gene_IGH_start.txt
     #merge length with the chromosome information
-    paste -d"\t" ${HOME}/gene_position/${line}/${line}_genes_IGH_productive_length.txt ${HOME}/gene_position/${line}/gene_IGH_start.txt > ${HOME}/gene_position/${line}/temp1.txt 
+    paste -d"\t" ${HOME}/gene_position/${function}/${line}/${line}_genes_IGH_length.txt ${HOME}/gene_position/${function}/${line}/gene_IGH_start.txt > ${HOME}/gene_position/${function}/${line}/temp1.txt 
     #add start and length to get gene end position
-    awk -F"\t" 'NR==1 {val = "End"} NR > 1 {val = $1+$3} {$(++NF) = val; print}' OFS='\t' ${HOME}/gene_position/${line}/temp1.txt > ${HOME}/gene_position/${line}/temp2.txt
+    awk -F"\t" 'NR==1 {val = "End"} NR > 1 {val = $1+$3} {$(++NF) = val; print}' OFS='\t' ${HOME}/gene_position/${function}/${line}/temp1.txt > ${HOME}/gene_position/${function}/${line}/temp2.txt
     #remove the Length column
-    cut -f2- ${HOME}/gene_position/${line}/temp2.txt > ${HOME}/gene_position/${line}/gene_IGH_pos.txt 
+    cut -f2- ${HOME}/gene_position/${function}/${line}/temp2.txt > ${HOME}/gene_position/${function}/${line}/gene_IGH_pos.txt 
     #remove temporary files
-    rm -rf ${HOME}/gene_position/${line}/temp*.txt 
+    rm -rf ${HOME}/gene_position/${function}/${line}/temp*.txt 
     ## Next we want to convert gene_IGH_pos.txt into a bed file
     # remove the headers
-    sed -i '1d' ${HOME}/gene_position/${line}/gene_IGH_pos.txt
+    sed -i '1d' ${HOME}/gene_position/${function}/${line}/gene_IGH_pos.txt
     # re-order file to convert into bed format
-    awk -F'\t' '{print $1 "\t" $2 "\t" $4 "\t" NR "\t" "0" "\t" $3 }' ${HOME}/gene_position/${line}/gene_IGH_pos.txt > ${HOME}/gene_position/${line}/gene_IGH_pos.bed
+    awk -F'\t' '{print $1 "\t" $2 "\t" $4 "\t" NR "\t" "0" "\t" $3 }' ${HOME}/gene_position/${function}/${line}/gene_IGH_pos.txt > ${HOME}/gene_position/${function}/${line}/gene_IGH_pos.bed
     #sort the bed file
-    sort -k1,1 -k2,2n -k3,3n ${HOME}/gene_position/${line}/gene_IGH_pos.bed > ${HOME}/gene_position/${line}/gene_IGH_pos_sorted.bed
+    sort -k1,1 -k2,2n -k3,3n ${HOME}/gene_position/${function}/${line}/gene_IGH_pos.bed > ${HOME}/gene_position/${function}/${line}/gene_IGH_pos_sorted.bed
 done
 
     
--- code/igv.sh
+++ code/igv.sh
@@ -14,26 +14,31 @@ source /spack/conda/miniconda3/4.12.0/etc/profile.d/conda.sh
 conda activate /home1/zhuyixin/.conda/envs/assembly
 
 function igv {
-    mkdir ${HOME}/igv/${1}
-    cat ${HOME}/gene_position/${1}/gene_IGH_pos_sorted.bed | while read chrom start end sv score strand
+    mkdir ${HOME}/igv/${function}/${1}
+    cat ${HOME}/gene_position/${function}/${1}/gene_IGH_pos_sorted.bed | while read chrom start end sv score strand
     do
     echo $sv
-    mkdir ${HOME}/igv/${1}/snapshot
+    mkdir ${HOME}/igv/${function}/${1}/snapshot
     bam="${HOME}/aligned_bam/${1}/${1}_merged_sorted.bam"
     echo "new"
     echo "preference SAM.SHOW_ALL_BASES 0"
-    echo "genome ${HOME}/assemblies/${1}*fasta"
+    echo "genome ${HOME}/assemblies/${1}.fasta"
     echo "snapshotDirectory ${HOME}/igv/${1}/snapshot"
     echo "load ${bam}"
-    echo "load ${HOME}/gene_position/${1}/gene_IGH_pos_sorted.bed"
+    echo "load ${HOME}/gene_position/${function}/${1}/gene_IGH_pos_sorted.bed"
     echo "goto ${chrom}:${start}-${end}"
     echo "snapshot ${1}_merged_${sv}.png"
-    done    > /home1/zhuyixin/sc1/ImmAssm/igv/${1}/igv.txt
+    done    > /home1/zhuyixin/sc1/ImmAssm/igv/${function}/${1}/igv.txt
 }
 
 HOME=/home1/zhuyixin/sc1/ImmAssm
-
+while getopts f: flag
+do
+    case "${flag}" in
+        f) function=${OPTARG};;
+    esac
+done
 cat ${HOME}/igv_name.txt | while read line
 do 
-    igv $line
+    igv $line $function
 done
