0022f2d430c8f5246a5e36ec229f180daf1a6461
zhuyixin43 <zhuyixin@github.com>
2023-08-21 14:15:52 -0700

    expanded the gene loci position to 40kB(+-20kB) region

--- code/dataPrepAutomated.sh
+++ code/dataPrepAutomated.sh
@@ -16,13 +16,20 @@ conda init
 source /spack/conda/miniconda3/4.12.0/etc/profile.d/conda.sh
 conda activate /home1/zhuyixin/.conda/envs/assembly
 
-
+while getopts g: flag
+do
+    case "${flag}" in
+        g) genome=${OPTARG};;
+    esac
+done
+echo $genome
 HOME=/home1/zhuyixin/sc1/ImmAssm
 cat ${HOME}/code/name.txt | while read line
 do 
     #check if this species' data is in bam or not, convert to fastq if yes
     count=`ls -1 ${HOME}/hifi_fastq/${line}/*.bam 2>/dev/null | wc -l`
-    if [ $count != 0 ]
+    merged=`ls -1 ${HOME}/hifi_fastq/${line}/*_merged.fastq 2>/dev/null | wc -l`
+    if [ $count != 0 ] && [ $merged == 0 ]
     then 
         for f in ${HOME}/hifi_fastq/${line}/*.bam ; do 
         (
@@ -33,19 +40,22 @@ do
         )&
         done ; wait
     fi
-    #unzip fastq gz file
-    gunzip ${HOME}/hifi_fastq/${line}/*.gz
-    #merge all the fastq file into a single file
-    cat ${HOME}/hifi_fastq/${line}/*.fastq > ${HOME}/hifi_fastq/${line}/${line}_merged.fastq
+    if [ $merged == 0 ]
+    then
+        #unzip fastq gz file
+        gunzip ${HOME}/hifi_fastq/${line}/*.gz
+        #merge all the fastq file into a single file
+        cat ${HOME}/hifi_fastq/${line}/*.fastq > ${HOME}/hifi_fastq/${line}/${line}_merged.fastq
+    fi
     #create output directories
-    mkdir /home1/zhuyixin/sc1/ImmAssm/aligned_sam/${line}
-    mkdir /home1/zhuyixin/sc1/ImmAssm/aligned_bam/${line}
+    mkdir ${HOME}/aligned_sam/${genome}/${line}
+    mkdir ${HOME}/aligned_bam/${genome}/${line}
     #map the merged fastq file to the coresponding assembly
-    minimap2 -t 20 -a /home1/zhuyixin/sc1/ImmAssm/assemblies/${line}*.fasta /home1/zhuyixin/sc1/ImmAssm/hifi_fastq/${line}/${line}_merged.fastq > /home1/zhuyixin/sc1/ImmAssm/aligned_sam/${line}/${line}_merged.sam
+    minimap2 -t 20 -a ${HOME}/assemblies/${line}*merged.fasta ${HOME}/hifi_fastq/${line}/${line}_merged.fastq > ${HOME}/aligned_sam/${genome}/${line}/${line}_merged.sam
     #convert the SAM result to sorted BAM format
-    samtools sort /home1/zhuyixin/sc1/ImmAssm/aligned_sam/${line}/${line}_merged.sam -o /home1/zhuyixin/sc1/ImmAssm/aligned_bam/${line}/${line}_merged_sorted.bam
+    samtools sort ${HOME}/aligned_sam/${genome}/${line}/${line}_merged.sam -o ${HOME}/aligned_bam/${genome}/${line}/${line}_merged_sorted.bam
     #index the sorted BAM file
-    samtools index /home1/zhuyixin/sc1/ImmAssm/aligned_bam/${line}/${line}_merged_sorted.bam
+    samtools index ${HOME}/aligned_bam/${genome}/${line}/${line}_merged_sorted.bam
 done
 
  
